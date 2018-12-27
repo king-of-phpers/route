e22fd251765072ef4c959dee1eb7e2e1ddc0dde1
zhaqq <qiuxiaoyong@linghit.com>
2018-12-27 19:03:45 +0800

    init

---
+++ src/Contracts/RouteInterface.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @author: ZhaQiu <34485431@qq.com>
+ * @time: 2018/12/27
+ */
+
+namespace KOF\Contracts;
+
+/**
+ * Interface RouteInterface
+ * @package KOF\Contracts
+ */
+interface RouteInterface
+{
+
+}
---
+++ src/Route.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @author: ZhaQiu <34485431@qq.com>
+ * @time: 2018/12/27
+ */
+
+namespace KOF\Route;
+
+class Route implements \KOF\Contracts\RouteInterface
+{
+
+}
